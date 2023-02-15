439fee71474fdf7cf7e270e96647721b5d7b55b4
Spark4545 <lerouxpaulhenri@gmail.com>
2023-02-15 06:14:02 -0800

    fixed syntax

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <unistd.h>
 
 /**
- * main - Prints "and that piece of art is useful\" - Dora Korpar, 2015-10-19", followed by a new line, to the standard error
+ * main - Prints "and that piece of art is useful\" - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error
  *
  * Return: always 1
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
