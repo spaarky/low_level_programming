49f91397d3edaf834783c46fc4afe02e99335564
Spark4545 <lerouxpaulhenri@gmail.com>
2023-02-15 06:09:09 -0800

    script 101

---
+++ hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "and that piece of art is useful\" - Dora Korpar, 2015-10-19", followed by a new line, to the standard error
+ *
+ * Return: always 1
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
