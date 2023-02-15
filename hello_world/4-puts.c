32f3f085669a23825748ed6423c336b51f4c3100
Spark4545 <lerouxpaulhenri@gmail.com>
2023-02-15 03:30:42 -0800

    added comment

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+
+/**
+ * Main - Print "Programming is like building a muiltilingual puzzle"
+ *
+ * Return : always 0
+ */
 int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
