5e27c0d8c3c7b705902915f6e54c172b18456233
Spark4545 <lerouxpaulhenri@gmail.com>
2023-02-20 05:11:52 -0800

    first throw t7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints alpahbet backwards
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a <= 'a'; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
