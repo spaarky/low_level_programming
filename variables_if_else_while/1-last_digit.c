115f2551a28ab1dbfaa94b212ebde20beb120f1b
Spark4545 <lerouxpaulhenri@gmail.com>
2023-02-20 02:53:36 -0800

    script 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -15,10 +15,10 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("Last digit of");
+	printf("Last digit of ");
 	if (n % 10 > 5)
 	{
-		printf("%d is greater than 5\n", n % 10);
+		printf("%d and is greater than 5\n", n % 10);
 	}
 	else if (n % 10 == 0)
 	{
@@ -26,7 +26,7 @@ int main(void)
 	}
 	else if (n % 10 != 0 && n % 10 < 6)
 	{
-	printf("%d is less than 6 and not 0\n", n % 10);
+	printf("%d and is less than 6 and not 0\n", n % 10);
 	}
 	return (0);
 }
