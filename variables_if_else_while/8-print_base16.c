d230de536ea6c9cfada08581c3309ebe9bb386ca
Spark4545 <lerouxpaulhenri@gmail.com>
2023-02-20 05:22:29 -0800

    first throw t8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints hexadecimal base
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	for (n = 'a'; n <= 'f'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
