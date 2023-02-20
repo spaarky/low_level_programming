a180ff54f7dfe563d99b48eeb2a4b6c7519d0a69
Spark4545 <lerouxpaulhenri@gmail.com>
2023-02-20 04:57:36 -0800

    first throw t6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print 0 to 9
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
