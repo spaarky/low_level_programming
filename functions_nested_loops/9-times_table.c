e79afd06521bd7b07bb49cfa9e0ecea700410f08
Spark4545 <lerouxpaulhenri@gmail.com>
2023-02-21 12:27:18 -0800

    first throw t9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -2,27 +2,31 @@
 #include <stdio.h>
 
 /**
- * times_table - prints the 9 times table, starting with 0
+ * times_table - print multiplication table up to 9.
  *
+ * Return: void
  *
  */
 
 void times_table(void)
 {
+
 	int row;
 	int column;
 	int product;
+
 
 	for (row = 0; row <= 9; row++)
 	{
 		for (column = 0; column <= 9; column++)
 		{
 			product = (row * column);
+
 			if (column == 0)
 			{
 				_putchar('0' + product);
 			}
-			else if (column <= 9)
+			else if (product <= 9)
 			{
 				_putchar(',');
 				_putchar(' ');
