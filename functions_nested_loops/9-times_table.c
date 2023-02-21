7d89bfa9a5cbda23d1547500e5c81abaa735c7f4
Spark4545 <lerouxpaulhenri@gmail.com>
2023-02-21 06:35:10 -0800

    first throw t9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -17,7 +17,7 @@ void times_table(void)
 	{
 		for (column = 0; column <= 9; column++)
 		{
-			product = (row + column);
+			product = (row * column);
 			if (column == 0)
 			{
 				_putchar('0' + product);
