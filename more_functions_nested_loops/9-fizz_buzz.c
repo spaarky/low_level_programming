809580ef5522b5e1dec698e77cbb6c8c30abb641
Spark4545 <lerouxpaulhenri@gmail.com>
2023-02-22 10:15:40 -0800

    fixed spaces

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -18,21 +18,24 @@ int main(void)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (n % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (n % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else
 		{
-			printf("%i ", n);
+			printf("%i", n);
 		}
-		
+		if (n < 100) 
+		{
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
