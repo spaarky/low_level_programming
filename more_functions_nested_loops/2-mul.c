69c26de975d4dd54e3f720487f3aa55f764eaab8
Spark4545 <lerouxpaulhenri@gmail.com>
2023-02-22 04:42:10 -0800

    first throw task 1

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - print the result of a multiplication of two integer
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of the multiplication of a with b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
