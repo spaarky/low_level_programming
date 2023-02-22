2e5d8a6cc404198dbb4763d598cf476eccda504a
Spark4545 <lerouxpaulhenri@gmail.com>
2023-02-22 09:45:51 -0800

    first throw task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - print a square made of #
+ * @size: size of the square
+ *
+ */
+
+void print_square(int size)
+{
+	int l;
+	int c;
+
+	if (size > 0)
+	{
+		for (c = 1; c <= size; c++)
+		{
+			for (l = 0; l <= size; l++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
