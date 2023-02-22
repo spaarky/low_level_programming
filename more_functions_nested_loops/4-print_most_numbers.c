b2cf48d5d69164f4df026b31de4958da84f187dd
Spark4545 <lerouxpaulhenri@gmail.com>
2023-02-22 05:16:28 -0800

    first throw task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int c;
+	
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c != '2' && c != '4')
+		{
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
+}
