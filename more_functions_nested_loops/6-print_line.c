95063ff9836f7bd2810f3b515c22b796b436ce03
Spark4545 <lerouxpaulhenri@gmail.com>
2023-02-22 05:59:51 -0800

    first throw

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		for (n = n; n > 0; n--)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
