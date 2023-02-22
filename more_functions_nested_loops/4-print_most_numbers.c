339babd00cf3d76c9a64db8c60e254a0f668fd96
Spark4545 <lerouxpaulhenri@gmail.com>
2023-02-22 11:25:54 -0800

    added description for betty

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,13 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
- *
+ * print_most_numbers - print
  */
 
 void print_most_numbers(void)
 {
 	int c;
-	
+
 	for (c = '0'; c <= '9'; c++)
 	{
 		if (c != '2' && c != '4')
