6933d60fb0e54e756a946cc66489941d9849bec6
Spark4545 <lerouxpaulhenri@gmail.com>
2023-02-22 05:38:24 -0800

    first throw task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+void more_numbers(void)
+{
+	int c;
+	int n;
+
+	for (c = 0; c <= 9; c++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n >= 10)
+			{
+				_putchar((n / 10) + '0');
+			}
+			_putchar((n % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
