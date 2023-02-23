0a371b65ea309753350f2007b18daefa8a30de2c
Spark4545 <lerouxpaulhenri@gmail.com>
2023-02-23 04:26:03 -0800

    hello world

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - prints
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int num1, num2, num3;
+
+	for (num1 = 0; num < 9; num1++)
+	{
+		for (num2 = num + 1; num2 <= 9; num2++)
+		{
+			for (num3 = num2 + 1; num3 <= 9; num3++)
+			{
+				putchar((num1) + '0');
+				putchar((num2) + '0');
+				putchar((num3) + '0');
+				{
+					if (num == 7)
+						break;
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
