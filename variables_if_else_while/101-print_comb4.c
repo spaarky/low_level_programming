590211e895552ad9df55bb269944f63dbbca084c
Spark4545 <lerouxpaulhenri@gmail.com>
2023-02-23 04:28:06 -0800

    added description for betty

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int num1, num2, num3;
 
-	for (num1 = 0; num < 9; num1++)
+	for (num1 = 0; num1 < 9; num1++)
 	{
-		for (num2 = num + 1; num2 <= 9; num2++)
+		for (num2 = num1 + 1; num2 <= 9; num2++)
 		{
 			for (num3 = num2 + 1; num3 <= 9; num3++)
 			{
@@ -20,7 +20,7 @@ int main(void)
 				putchar((num2) + '0');
 				putchar((num3) + '0');
 				{
-					if (num == 7)
+					if (num1 == 7)
 						break;
 					{
 						putchar(',');
