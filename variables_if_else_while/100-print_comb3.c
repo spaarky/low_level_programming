6dea46eaae3273d979cbb78c2ffb89e702e2ac1f
Spark4545 <lerouxpaulhenri@gmail.com>
2023-02-23 01:33:04 -0800

    first try

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 < 9; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 9; num2++)
+		{
+			putchar(num1);
+			putchar(num2);
+			{
+				if (num1 == 8)
+					break;
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
