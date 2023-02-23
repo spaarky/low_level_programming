1d6afd8f455f54e4df8339a74957d54048c6640e
Spark4545 <lerouxpaulhenri@gmail.com>
2023-02-23 06:10:45 -0800

    fix

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -18,9 +18,7 @@ void print_times_table(int n)
 				product = (row * column);
 
 				if (column == 0)
-				{
 					_putchar('0' + product);
-				}
 				else if (product <= 9)
 				{
 					_putchar(',');
