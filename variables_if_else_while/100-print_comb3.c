e70bfb3211e0d63cffd4a81fee14bce0ccf9e485
Spark4545 <lerouxpaulhenri@gmail.com>
2023-02-23 04:02:57 -0800

    fixed putchar

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -13,8 +13,8 @@ int main(void)
 	{
 		for (num2 = num1 + 1; num2 <= 9; num2++)
 		{
-			putchar(num1);
-			putchar(num2);
+			putchar((num1) + 0);
+			putchar((num2) + 0);
 			{
 				if (num1 == 8)
 					break;
