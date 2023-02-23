961196bb42555dc8949f31cf48535e96258cd1de
Spark4545 <lerouxpaulhenri@gmail.com>
2023-02-23 04:55:04 -0800

    aled

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 100; num1++)
+	{
+		for (num2 = num1 + 1; num2 < 100; num2++)
+		{
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
