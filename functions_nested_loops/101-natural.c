717134f6e866e1fa95b5efabc43e7d8a80c5d28d
Spark4545 <lerouxpaulhenri@gmail.com>
2023-02-23 06:24:56 -0800

    fixed name

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+	int num, sum;
+
+	for (num = 1; num < 1024; num++)
+	{
+		if (num % 3 == 0)
+		{
+			sum = sum + num;
+		}
+		else if (num % 5 == 0)
+		{
+			sum = sum + num;
+		}
+	}
+	printf("%i\n", sum);
+	return (0);
+}
