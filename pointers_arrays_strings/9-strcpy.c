7c5347cc92e97b0a7e30cfbdcc06f6fc7e83c2c5
Spark4545 <lerouxpaulhenri@gmail.com>
2023-02-27 18:12:10 +0100

    task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strlen - give the lengh of a string
+ * @s: the string
+ *
+ * Return: the lengh of s
+ */
+
+int _strlen(char *s)
+{
+	int lengh = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		lengh++;
+	}
+	return (lengh);
+}
+
+/**
+ * _strcpy copies the string pointed by src into dest
+ * @dest: destination
+ * @src: source
+ *
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+    int id = 0;
+
+    for (; id <= _strlen(src); id++)
+        dest[id] = src[id];
+    
+    return(dest);
+}
