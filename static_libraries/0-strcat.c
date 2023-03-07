875a699be771663a43f91d0bda50bc4e98944431
Spark4545 <124456232+Spark4545@users.noreply.github.com>
2023-03-07 20:00:03 +0100

    Create 0-strcat.c

---
+++ static_libraries/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: string to be append to
+ * @src: strind to append
+ *
+ * Return: concatenated string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a = 0, b = 0;
+
+	while (dest[a] != '\0')
+		a++;
+
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+
+	return (dest);
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
