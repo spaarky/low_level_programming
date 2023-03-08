d2bef47e5aad29607362abb67911f09069f32e79
Spark4545 <lerouxpaulhenri@gmail.com>
2023-03-08 10:41:42 +0100

    task 3 betty

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -3,16 +3,19 @@
 #include "main.h"
 
 /**
+ * main - multiplies 2 integers
+ * @argc: argument counter
+ * @argv: argumet mult
  *
+ * Return: 0 or 1
  */
-
 int main(int argc, char *argv[])
 {
-        if (argc != 3)
-        {
-                printf("Error\n");
-                return(1);
-        }
-        printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-        return(1);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
