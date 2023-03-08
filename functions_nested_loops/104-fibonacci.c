209fd2f012e8149554119998364ebfcc485d1701
Spark4545 <lerouxpaulhenri@gmail.com>
2023-03-08 19:12:16 +0100

    task104 fixed

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 /**
+ * main - print first 98 Fibonacci numbers without using long long, malloc,
+ * pointers, array/tables, or structures
  *
+ * Return: 0
  */
 
 int main(void)
@@ -14,14 +17,15 @@ int main(void)
 
 	printf("1");
 
-	for (count = 2; count < 93; count++)
+	for (count = 2; count < 93; count++) /* will still print */
 	{
 		sum = a + b;
 		a = b;
 		b = sum;
 		printf(", %lu", sum);
 	}
-	a_head = a / 1000000000;
+
+	a_head = a / 1000000000; /* break larger num into 2 parts */
 	a_tail = a % 1000000000;
 	b_head = b / 1000000000;
 	b_tail = b % 1000000000;
@@ -29,7 +33,7 @@ int main(void)
 	for (; count < 99; count++)
 	{
 		overflow = (a_tail + b_tail) / 1000000000;
-		sum_tail = (a_tail + b_tail) - (a_tail + b_tail * overflow);
+		sum_tail = (a_tail + b_tail) - (1000000000 * overflow);
 		sum_head = (a_head + b_head) + overflow;
 
 		printf(", %lu%lu", sum_head, sum_tail);
@@ -39,6 +43,9 @@ int main(void)
 		b_head = sum_head;
 		b_tail = sum_tail;
 	}
+
+
 	printf("\n");
+
 	return (0);
 }
