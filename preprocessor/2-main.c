2cecc8fe409224dddbe0d1683b52fa6fa1e54a05
Spark4545 <lerouxpaulhenri@gmail.com>
2023-03-13 17:47:20 +0100

    better betty coding style

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - print the name of the file it was compiled from, followed by a new line
+ * main - print the name of the file it was compiled from,
+ * followed by a new line
  *
  * Return: always 0
  */
