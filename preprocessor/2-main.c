d80781d2a68827f13ea44bc88ba47ad060322bcd
Spark4545 <lerouxpaulhenri@gmail.com>
2023-03-13 17:46:22 +0100

    betty coding style added

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - print the name of the file it was compiled from, followed by a new line
+ *
+ * Return: always 0
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
