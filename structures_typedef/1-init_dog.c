4ae0dda653cd8f4f0063c157587a1693824af6f5
Spark4545 <lerouxpaulhenri@gmail.com>
2023-03-20 13:33:34 +0100

    betty

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -2,7 +2,11 @@
 #include "dog.h"
 
 /**
- *
+ * init_dog - initialize a variable of type struct dog
+ * @d: a variable of type struct dog
+ * @name: member
+ * @age: member
+ * @owner: member
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
