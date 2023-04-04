b311730183f6bfb19814280ed4c98dd466dbec0c
Spark4545 <lerouxpaulhenri@gmail.com>
2023-04-04 12:42:16 +0000

    task 2

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ *
+*/
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(struct dlistint_s));
+
+	if (!new)
+		return (NULL);
+	new->n = n;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		new->next = NULL;
+		new->prev = NULL;
+		return (new);
+	}
+
+	new->next = *head;
+	new->prev = NULL;
+	(*head)->prev = new;
+	*head = new;
+	return (new);
+}
