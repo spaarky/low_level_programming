a9c1cf4d2f3c518d6254b7c9cfd0b3fa1cfc12b0
Spark4545 <lerouxpaulhenri@gmail.com>
2023-04-04 08:21:56 +0000

    Betty style

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,7 +1,10 @@
 #include "lists.h"
 
 /**
+ * print_list - print all elements of list_t list
+ * @h: linked list
  *
+ * Return: number of nodes in linked list
 */
 
 size_t print_list(const list_t *h)
