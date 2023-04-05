e74c3b5bcbcf89316f7582dc96a4fc27d6406361
Spark4545 <lerouxpaulhenri@gmail.com>
2023-04-05 14:12:47 +0000

    task 0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	int length = 0, index = 0;
+	unsigned int sum = 0;
+
+	if (b == NULL)
+		return (sum);
+
+	while (b[length] != '\0')
+		length++;
+
+	length -= 1;
+
+	while (b[index])
+	{
+		if ((b[index] != '0') && (b[index] != '1'))
+			return (sum);
+
+		if (b[index] == '1')
+			sum += (1 * (1 << length));
+
+		index++;
+		length--;
+	}
+	return (sum);
+}
