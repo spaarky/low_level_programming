59b8e68c68a54bddc5b5419f5a6fb80ad4dc8e78
Spark4545 <lerouxpaulhenri@gmail.com>
2023-04-05 12:24:24 +0000

    syntax fix

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -27,7 +27,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while (index != 0) && (delete->next != NULL)
+	while ((index != 0) && (delete->next != NULL))
 	{
 		index -= 1;
 		delete = delete->next;
