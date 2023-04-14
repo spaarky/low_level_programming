695e98c1f9b0207a4e4ad7144ece35644b9388d0
Spark4545 <lerouxpaulhenri@gmail.com>
2023-04-14 19:40:00 +0000

    betty

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -8,7 +8,7 @@
  *
  * Return: 0 on success
  */
-int __exit(int error, char *s, int fileDescriptor)
+int __exit(int error, char *s, int fd)
 {
 	switch (error)
 	{
@@ -22,7 +22,7 @@ int __exit(int error, char *s, int fileDescriptor)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
 		exit(error);
 	case 100:
-		dprintf(STDERR_FILENO, "Error: Can't close fileDescriptor %d\n", fileDescriptor);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(error);
 	default:
 		return (0);
@@ -32,37 +32,37 @@ int __exit(int error, char *s, int fileDescriptor)
 /**
  * main - copies one file to another
  * @argc: should be 3 (./a.out copyfromfile copytofile)
- * @argv: first is file to copy from (fileDescriptor_1), second is file to copy to (fileDescriptor_2)
+ * @argv: first is file to copy from fd1, second is file to copy to fd2
  *
  * Return: 0 (success), 97-100 (exit value errors)
  */
 int main(int argc, char *argv[])
 {
-	int fileDescriptor_1, fileDescriptor_2, nRead, nWrite;
+	int fd1, fd2, nRead, nWrite;
 	char *buffer[1024];
 
 	if (argc != 3)
 		__exit(97, NULL, 0);
 
-	fileDescriptor_2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (fileDescriptor_2 == -1)
+	fd2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (fd2 == -1)
 		__exit(99, argv[2], 0);
 
-	fileDescriptor_1 = open(argv[1], O_RDONLY);
-	if (fileDescriptor_1 == -1)
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
 		__exit(98, argv[1], 0);
 
-	while ((nRead = read(fileDescriptor_1, buffer, 1024)) != 0)
+	while ((nRead = read(fd1, buffer, 1024)) != 0)
 	{
 		if (nRead == -1)
 			__exit(98, argv[1], 0);
 
-		nWrite = write(fileDescriptor_2, buffer, nRead);
+		nWrite = write(fd2, buffer, nRead);
 		if (nWrite == -1)
 			__exit(99, argv[2], 0);
 	}
 
-	close(fileDescriptor_2) == -1 ? (__exit(100, NULL, fileDescriptor_2)) : close(fileDescriptor_2);
-	close(fileDescriptor_1) == -1 ? (__exit(100, NULL, fileDescriptor_1)) : close(fileDescriptor_1);
+	close(fd2) == -1 ? (__exit(100, NULL, fd2)) : close(fd2);
+	close(fd1) == -1 ? (__exit(100, NULL, fd1)) : close(fd1);
 	return (0);
 }
