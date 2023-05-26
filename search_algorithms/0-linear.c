d3cb96bea8f21d32652b0dbcbd610d83ba39f5ac
Spark4545 <lerouxpaulhenri@gmail.com>
2023-05-26 05:31:48 -0700

    all tasks

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,29 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - search lnearly
+ * @array: given array of int
+ * @size: size of array
+ * @value: value to search for
+ *
+ * Return: index at which value i!qs found
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t index;
+	index = 0;
+
+	if (!array)
+		return (-1);
+
+	while (index < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", index, array[index]);
+		if (array[index] == value)
+			return (index);
+
+		index++;
+	}
+	return (-1);
+}
